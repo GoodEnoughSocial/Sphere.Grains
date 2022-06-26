22dba2127da26b68cce6295e5dd717c553c5cc93
Chad Jessup <chad.jessup@gmail.com>
2022-06-25 19:27:26 -0700

    servicediscoverygrain

---
+++ ServiceDiscoveryGrain.cs
@@ -0,0 +1,19 @@
+﻿using Orleans;
+using Sphere.Interfaces;
+using Sphere.Shared;
+
+namespace Sphere.Grains;
+
+public class ServiceDiscoveryGrain : Grain, IServiceDiscovery
+{
+    private ServiceDefinition? _serviceDefinition;
+
+    public Task<ServiceDefinition?> GetServiceDefinition()
+        => Task.FromResult(_serviceDefinition);
+
+    public Task<ServiceDefinition> SetServiceDefinition(ServiceDefinition serviceDefinition)
+    {
+        _serviceDefinition = serviceDefinition;
+        return Task.FromResult(_serviceDefinition);
+    }
+}

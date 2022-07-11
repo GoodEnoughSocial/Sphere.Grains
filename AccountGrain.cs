9b054214de5aad1b5576916a5d1ca0b425aff0ed
Chad Jessup <chad.jessup@gmail.com>
2022-07-11 00:59:52 -0700

    basic persistent grains for account/profiles

---
+++ AccountGrain.cs
@@ -0,0 +1,42 @@
+﻿using Orleans;
+using Orleans.Runtime;
+using Sphere.Interfaces;
+using Sphere.Shared.Models;
+
+namespace Sphere.Grains;
+
+public class AccountGrain : Grain, IAccount
+{
+    private readonly IPersistentState<AccountState> account;
+
+    public AccountGrain(
+        [PersistentState("accounts", "accountStore")]
+        IPersistentState<AccountState> persistedAccount)
+    {
+        account = persistedAccount;
+    }
+
+    public async Task<AccountState> RegisterAccount(AccountState newAccount)
+    {
+        // TODO: some validation and whatnot
+        account.State = newAccount;
+
+        await account.WriteStateAsync();
+
+        return account.State;
+    }
+
+    public Task<AccountState> GetAccountState()
+    {
+        return Task.FromResult(account.State);
+    }
+
+    public async Task<AccountState> UpdateAccount(AccountState updatedAccount)
+    {
+        account.State = updatedAccount;
+
+        await account.WriteStateAsync();
+
+        return account.State;
+    }
+}
---
+++ ProfileGrain.cs
@@ -0,0 +1,31 @@
+﻿using Orleans;
+using Orleans.Runtime;
+using Sphere.Interfaces;
+using Sphere.Shared.Models;
+
+namespace Sphere.Grains;
+
+public class ProfileGrain : Grain, IUserProfile
+{
+    private readonly IPersistentState<UserProfileState> profile;
+
+    public ProfileGrain(
+        [PersistentState("profiles", "profilesStore")]
+        IPersistentState<UserProfileState> persistedAccount)
+    {
+        profile = persistedAccount;
+    }
+
+    public Task<UserProfileState> GetProfile()
+    {
+        return Task.FromResult(profile.State);
+    }
+
+    public async Task<UserProfileState> Update(UserProfileState updatedProfile)
+    {
+        profile.State = updatedProfile;
+        await profile.WriteStateAsync();
+
+        return profile.State;
+    }
+}
